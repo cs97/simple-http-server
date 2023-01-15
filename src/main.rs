6887c340ae044abb10b7d2074c5444425805b986
cs97 <chrswr97@gmail.com>
2023-01-15 23:51:17 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ use crate::status_codes::*;
 
 fn main() {
 
-    //let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let listener = TcpListener::bind("192.168.178.53:7878").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    //let listener = TcpListener::bind("192.168.178.53:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         handle_connection(stream);
